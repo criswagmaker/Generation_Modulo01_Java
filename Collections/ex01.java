a1e96077915b12333a7c7362fc9f7047a2e503aa
Cristian Wagmaker <cristian.wagmakerpessoal@gmail.com>
2022-03-10 14:49:26 -0300

    Corrigindo Collections

--- Collections/ex01.java
+++ Collections/ex01.java
@@ -1,7 +1,5 @@
 package Collections;
 
-import java.util.Scanner;
-
 import java.util.ArrayList;
 
 import java.util.Collection;
@@ -12,44 +10,38 @@ public class ex01 {
 	public static void main(String[] args) {
 
 		ArrayList<String> estoque = new ArrayList<>();
-		
-		Scanner sc = new Scanner(System.in);
-				
+
 		int resposta;
-		//adicionando elementos
+		// adicionando elementos
 		System.out.println("Adicionando elementos:");
 		estoque.add("Estomazil");
 		estoque.add("Dipirona");
 		estoque.add("Ibuprofeno");
 		estoque.add("Benegripe");
-		Collections.sort(estoque);
+
 		System.out.println(estoque);
 		System.out.println();
-		
-	
-		
-		//inserindo elementos
+
+		// inserindo elementos
 		System.out.println("Inserindo/Atualizando elementos:");
-		estoque.set(3, "Rivotril"); 
+		estoque.set(3, "Rivotril");
 		estoque.set(1, "Paracetamol");
-		Collections.sort(estoque);
+
 		System.out.println(estoque);
 		System.out.println();
-		
-		//removendo elementos
+
+		// removendo elementos
 		System.out.println("\nRemovendo elementos:");
 		estoque.remove(2);
 		estoque.remove(1);
-		Collections.sort(estoque);
+
 		System.out.println(estoque);
 		System.out.println();
-		
-		//remove todos os dados da Array
+
+		// remove todos os dados da Array
 		System.out.println("Removendo todos os dados do Array:");
 		estoque.clear();
 		System.out.println(estoque);
-		
-		
 
 	}
 }
